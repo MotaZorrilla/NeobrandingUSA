388e18d0f606f5b014f8ca66cb4c2827317ca768
hector <hectormotazorrilla@gmail.com>
2023-10-30 20:10:22 -0400

    PAgina Neobranding USA

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,93 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+      {{-- <div class="footer-newsletter">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6">
+            <h4>Unete a Nuestro Boletín</h4>
+            <p>¡No te pierdas ninguna novedad ni promoción! Suscríbete a nuestro boletín y mantente informado sobre las últimas tendencias y noticias del mundo del marketing digital.</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div> --}}
+
+      <div class="footer-top">
+          <div class="container">
+              <div class="row d-flex justify-content-center">
+
+                  <div class="col-lg-4 col-md-6 footer-contact">
+                      <h3>Neobranding Venezuela</h3>
+                      <p>
+                          Ciudad Guayana <br>
+                          Estado Bolívar<br>
+                          Venezuela <br><br>
+                          <strong>Phone:</strong> +58 414-9985973<br>
+                          <strong>Email:</strong> <a href="mailto:info@neobranding.com.ve">
+                              info@neobranding.com.ve</a><br>
+                      </p>
+                  </div>
+                    {{-- <div class="col-lg-2 col-md-6 footer-contact">
+                    </div> --}}
+                  <div class="col-lg-4 col-md-6 footer-contact">
+                      <h3>Neobranding Chile</h3>
+                      <p>
+                          Gran Concepción <br>
+                          Región del Bio Bio<br>
+                          Chile <br><br>
+                          <strong>Phone:</strong> +56 9322 77455<br>
+                          <strong>Email:</strong> <a href="mailto:info@neobranding.cl">
+                              info@neobranding.cl</a><br>
+                      </p>
+                  </div>
+                              {{-- <div class="col-lg-4 col-md-6 footer-links">
+                        <h4>Nuestros Servicios</h4>
+                        <ul>
+                          <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+                          <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+                          <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+                          <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+                          <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+                        </ul>
+                      </div> --}}
+
+                  <div class="col-lg-4 col-md-6 footer-contact "> 
+                        {{-- footer-links --}}
+                      <h3>Nuestras Redes Sociales</h3>
+                      <p>Síguenos en nuestras redes sociales para estar al día <br>de nuestras últimas noticias y
+                          promociones.</p><br><br>
+                      <div class="social-links mt-3">
+                          {{-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>  --}}
+                          <a href="https://www.instagram.com/neobranding.agency/" class="instagram"><i
+                                  class="bx bxl-instagram"></i></a>
+                          <a href="https://www.facebook.com/profile.php?id=100063775674604" class="facebook"><i
+                                  class="bx bxl-facebook"></i></a>
+
+                          {{-- <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a> --}}
+                      </div>
+                  </div>
+
+              </div>
+          </div>
+      </div>
+
+      <!-- ======= Clients Section ======= -->
+      {{-- @include('clients') --}}
+
+      <div class="container footer-bottom clearfix">
+          <div class="copyright">
+              &copy; Copyright <strong><span>Neobranding</span></strong>. All Rights Reserved
+          </div>
+          <div class="credits">
+              <!-- All the links in the footer should remain intact. -->
+              <!-- You can delete the links only if you purchased the pro version. -->
+              <!-- Licensing information: https://bootstrapmade.com/license/ -->
+              <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
+              Designed by <a href="https://motazorrilla.com/">@MotaZorrilla</a>
+          </div>
+      </div>
+  </footer><!-- End Footer -->
---
+++ resources/views/team.blade.php
@@ -0,0 +1,118 @@
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team section-bg">
+        <div class="container" data-aos="fade-up">
+  
+          <div class="section-title">
+            <h2>Equipo</h2>
+            <p>Contamos con un grupo de profesionales altamente capacitados en diferentes áreas del marketing digital, 
+              desde la estrategia y planificación hasta la ejecución y análisis de resultados. Conoce a algunos de ellos</p>
+          </div>
+  
+          <div class="row">
+  
+            <div class="col-lg-6">
+              <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+                <div class="pic"><img src="assets/img/team/rene.jpg" class="img-fluid" alt=""></div>
+                <div class="member-info">
+                  <h4>René Ambiado</h4>
+                  <span>Chief Executive Officer</span>
+                  <p>Con más de dos décadas de experiencia en el marketing digital, René lidera nuestro equipo con sabiduría y visión estratégica.</p>
+                  {{-- <div class="social">
+                    <a href=""><i class="ri-twitter-fill"></i></a>
+                    <a href=""><i class="ri-facebook-fill"></i></a>
+                    <a href=""><i class="ri-instagram-fill"></i></a>
+                    <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                  </div> --}}
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-lg-6 mt-4 mt-lg-0">
+              <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="200">
+                <div class="pic"><img src="assets/img/team/liz.jpg" class="img-fluid" alt=""></div>
+                <div class="member-info">
+                  <h4>Liz Ruiz</h4>
+                  <span>Content Manager</span>
+                  <p>Licenciada en derecho y dotada de una actitud proactiva y disciplinada, Liz garantiza que el contenido sea preciso y efectivo.</p>
+                  {{-- <div class="social">
+                    <a href=""><i class="ri-twitter-fill"></i></a>
+                    <a href=""><i class="ri-facebook-fill"></i></a>
+                    <a href=""><i class="ri-instagram-fill"></i></a>
+                    <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                  </div> --}}
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-lg-6 mt-4">
+              <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="300">
+                <div class="pic"><img src="assets/img/team/hector.jpg" class="img-fluid" alt=""></div>
+                <div class="member-info">
+                  <h4>Héctor Mota</h4>
+                  <span>Chief Technology Officer</span>
+                  <p>Ingeniero y apasionado de las tecnologías de la información, Héctor lidera nuestra infraestructura TI con un enfoque en la innovación.</p>
+                  {{-- <div class="social">
+                    <a href=""><i class="ri-twitter-fill"></i></a>
+                    <a href=""><i class="ri-facebook-fill"></i></a>
+                    <a href=""><i class="ri-instagram-fill"></i></a>
+                    <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                  </div> --}}
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-lg-6 mt-4">
+              <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="400">
+                <div class="pic"><img src="assets/img/team/yetzibel.jpg" class="img-fluid" alt=""></div>
+                <div class="member-info">
+                  <h4>Yetzibel España</h4>
+                  <span>Community Manager</span>
+                  <p>Con su carisma y atención al público, Yetzibel se encarga de construir relaciones sólidas entre nuestra marca y nuestra audiencia.</p>
+                  {{-- <div class="social">
+                    <a href=""><i class="ri-twitter-fill"></i></a>
+                    <a href=""><i class="ri-facebook-fill"></i></a>
+                    <a href=""><i class="ri-instagram-fill"></i></a>
+                    <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                  </div> --}}
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-lg-6 mt-4">
+              <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="400">
+                <div class="pic"><img src="assets/img/team/simon.jpg" class="img-fluid" alt=""></div>
+                <div class="member-info">
+                  <h4>Simón Boada</h4>
+                  <span>Director de Marketing</span>
+                  <p>Siempre enfocado en soluciones, Simón lidera nuestras estrategias de marketing con un enfoque en el crecimiento y el éxito del cliente.</p>
+                  {{-- <div class="social">
+                    <a href=""><i class="ri-twitter-fill"></i></a>
+                    <a href=""><i class="ri-facebook-fill"></i></a>
+                    <a href=""><i class="ri-instagram-fill"></i></a>
+                    <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                  </div> --}}
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-lg-6 mt-4">
+              <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="400">
+                <div class="pic"><img src="assets/img/team/gregorio.jpg" class="img-fluid" alt=""></div>
+                <div class="member-info">
+                  <h4>Gregorio España</h4>
+                  <span>Diseñador Gráfico</span>
+                  <p>Creativo, minucioso y eficiente, Gregorio aporta una estética visual excepcional a nuestro contenido y marca.</p>
+                  {{-- <div class="social">
+                    <a href=""><i class="ri-twitter-fill"></i></a>
+                    <a href=""><i class="ri-facebook-fill"></i></a>
+                    <a href=""><i class="ri-instagram-fill"></i></a>
+                    <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                  </div> --}}
+                </div>
+              </div>
+            </div> 
+  
+          </div>
+  
+        </div>
+      </section><!-- End Team Section -->
